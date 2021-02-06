cf553ce1393d626ad1803c596b75f7bb1136d382
Joshua Galit <joshuaimalay@gmail.com>
2021-02-06 22:54:04 +0800

    separate layout pages into folder

---
+++ layouts/default.js
@@ -0,0 +1,18 @@
+import Footer from "../components/Footer"
+import NavBar from "../components/Navbar"
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex h-screen bg-gray-200">
+      <div className="flex flex-col justify-between max-w-sm mx-auto bg-white shadow-lg rounded-md">
+        <NavBar />
+        <main className="flex-1 overflow-y-scroll">
+          { children }
+        </main>
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Layout
