14c87691708ff31a398421f2d6e8934160a8b2fe
Joshua Galit <joshuaimalay@gmail.com>
2021-02-07 10:11:07 +0800

    add w-full to maintain maximum defaut width

--- layouts/default.js
+++ layouts/default.js
@@ -4,7 +4,7 @@ import NavBar from "../components/Navbar"
 const Layout = ({ children }) => {
   return (
     <div className="flex h-screen bg-gray-200">
-      <div className="flex flex-col justify-between max-w-sm mx-auto bg-white shadow-lg rounded-md">
+      <div className="flex flex-col justify-between max-w-sm w-full mx-auto bg-white shadow-lg rounded-md">
         <NavBar />
         <main className="flex-1 overflow-y-scroll">
           { children }
