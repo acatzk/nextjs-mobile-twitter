b006cdfe0090432a7efc1d80d221402f4d50f2c3
Joshua Galit <joshuaimalay@gmail.com>
2021-02-10 15:31:57 +0800

    add header main and footer tag in main mobile twitter

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
-import Layout from '../layouts/default'
+import Footer from "../components/Footer"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className="flex h-screen bg-gray-200">
+      <div className="flex flex-col justify-between max-w-sm w-full mx-auto bg-white shadow-lg rounded-md">
+        <header className="flex items-center px-4 py-3 border-b">
+          <img  className="rounded-full w-7 h-7" 
+                src="https://pbs.twimg.com/profile_images/1250989900964360192/TxsfrCBi_400x400.jpg"
+                />
+          <span className="ml-6 text-lg font-extrabold">{ Component.headerTitle }</span>
+        </header>
+        <main className="flex-1 overflow-y-scroll">
+          <Component { ...pageProps } />
+        </main>
+        <Footer />
+      </div>
+    </div>
   )
 }
 
