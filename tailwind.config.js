94a32638f17ca80612a9dc495082eda4cf63f5a6
Joshua Galit <joshuaimalay@gmail.com>
2021-02-19 12:46:05 +0800

    refactor: remove unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}', 
-    './src/components/**/*.{js,ts,jsx,tsx}'
-  ],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
